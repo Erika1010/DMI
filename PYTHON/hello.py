b748da61278b5f97676a604efbb2281073c1d5e6
dcr1586@inbox.lv <erika3303@inbox.lv>
2017-11-02 12:00:12 +0200

    20171102_12_00

---
+++ PYTHON/hello.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#https://www.tutorialspoint.com/unix_commands/echo.htm
+#Example 3. -> 6.
+# echo -e '...' [SHELL] -> print "..." [PYTHOM]
+'''
+print "Hello World!"
+'''
+
+'''
+# Example 3
+print "Here \bthe \bspaces \bare \bbackspaced."
+'''
+
+'''
+# Example 4 
+print "Here \nthe \nspaces \nare \nnewlined."
+'''
+
+'''
+# Example 5
+print  "Here \tthe \tspaces \thave \thorizontal \ttab \tspaces."
+'''
+
+
+# Example 6
+print "Here \vSmaržo \vceriņi \vun \vzeme \vKad \vlietus \vzāli \vmitrina \vTu  \vsēdi  \včetrās  \vsienās \vUn \vdiena  \vvelkās  \vgara."
+print  "\t\t\t\t\t\t\t\tHere \nsmaržo \nceriņi \nun \nzeme \nKad \nlietus \nzāli \tmitrina tvtu \tsēdi  \včetras vsienās  \vun \vdiena \vvelkās \ngara." 
+
+
+
+'''
+# Example 7 
+print "Here the \rcontent before this is removed/"
+'''
+
+
+
