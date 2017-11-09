c7309f11d7ef5feb8d4af7e180881b34e59caf87
lionika88@mail.ru <erika3303@inbox.lv>
2017-11-09 09:55:12 +0200

    20171102_12_00

---
+++ PYTHON/variables.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+a = 65;
+print type (a)
+#http://www.cplusplus.com/reference/cstdio/printf/?kw=printf
+print "Mainīga a vertība kā dec skaitlis ir: %d"%(a)
+print "Mainīga a vertība kā hex skaitlis ir: %x"%(a)
+print "Mainīga a vertība kā oct skaitlis ir: %o"%(a)
+print "Mainīga a vertība kā simbols ir: %c"%(a)
+a = 'A';
+print type (a)
+print "Mainīga a vertība kā dec skaitlis ir: %d"%(ord(a))
+print "Mainīga a vertība kā hex skaitlis ir: %x"%(ord(a))
+print "Mainīga a vertība kā oct skaitlis ir: %o"%(ord(a))
+print "Mainīga a vertība kā simbols ir: %c"%(a)
+a = '\n';
+print type (a)
+print "Mainīga a vertība kā dec skaitlis ir: %d"%(ord(a))
+print "Mainīga a vertība kā hex skaitlis ir: %x"%(ord(a))
+print "Mainīga a vertība kā oct skaitlis ir: %o"%(ord(a))
+print "Mainīga a vertība kā simbols ir: %c"%(a)
+a = 8.5
+print type (a)
+print "Mainīga a vertība kā dec skaitlis ir: %d"%(a)
+print "Mainīga a vertība kā hex skaitlis ir: %x"%(a)
+print "Mainīga a vertība kā oct skaitlis ir: %o"%(a)
+#print "Mainīga a vertība kā simbols ir: %c"%(a)
+print "Mainīga a vertība kā reāls  skaitlis ir: %10.3f"%(a)
+
+
